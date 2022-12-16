72b51b0bda708bb72019b5a37da389faee298f09
Aymalla <Aymalla@outlook.com>
2022-12-16 16:50:39 +0000

    unit-test

--- data-reconciliation-app/src/utils/dependencies.ts
+++
@@ -1,5 +0,0 @@
-import * as ccfapp from "@microsoft/ccf-app";
-import { ReconciledRecord } from "../models/reconciled-record";
-
-const kvStore:ccfapp.TypedKvMap<string, ReconciledRecord> = null;// = typedKv("data", ccfapp.string, json<ReconciledRecord>());
-export {kvStore}
