1407a6b8942a6e3effd4999fedc04f940254df8c
Mahmut Canga <cangamahmut@gmail.com>
2022-12-20 17:35:39 +0000

    created endpoint for get report by id

--- data-reconciliation-app/src/endpoints/reporting.ts
+++ data-reconciliation-app/src/endpoints/reporting.ts
@@ -14,5 +14,25 @@ export function getHandler(request: ccfapp.Request<any>): ccfapp.Response<CCFRes
   if (isValidIdentity.failure || !isValidIdentity.content) return ApiResult.AuthFailure();
 
   const response = reportingService.getData(callerId);
+  if (response.failure) return ApiResult.Failed(response);
+
+  return ApiResult.Succeeded(response);
+}
+
+export function getByIdHandler(request: ccfapp.Request<any>): ccfapp.Response<CCFResponse> {
+  console.log("Reporting endpoint called");
+  const getCallerId = authenticationService.getCallerId(request);
+  if (getCallerId.failure) return ApiResult.Failed(getCallerId);
+
+  const callerId = getCallerId.content;
+
+  const isValidIdentity = authenticationService.isValidIdentity(callerId);
+  if (isValidIdentity.failure || !isValidIdentity.content) return ApiResult.AuthFailure();
+
+  const key = request.params['id'];
+
+  const response = reportingService.getDataById(callerId, key);
+  if (response.failure) return ApiResult.Failed(response);
+
   return ApiResult.Succeeded(response);
 }
