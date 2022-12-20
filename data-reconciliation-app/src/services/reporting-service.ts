84adb4a8e79947faf8f4ddb6afe5e3aa385d248d
Mahmut Canga <cangamahmut@gmail.com>
2022-12-20 17:35:52 +0000

    connect service to domain model for reporting

--- data-reconciliation-app/src/services/reporting-service.ts
+++ data-reconciliation-app/src/services/reporting-service.ts
@@ -1,17 +1,49 @@
 import { ReconciledRecord } from "../models/reconciled-record";
 import { ServiceResult } from "../utils/service-result";
 import keyValueRepository, { IRepository } from "../repositories/kv-repository";
+import { ApiResult } from "../utils/api-result";
+import { DataConcilationReport } from "../models/data-conciliation-report";
 
 export interface IReportingService {
-  getData(userId: string): ServiceResult<object>;
+  getData(userId: string): ServiceResult<DataConcilationReport>;
+  getDataById(userId: string, key: string): ServiceResult<DataConcilationReport>;
 }
 
 export class ReportingService implements IReportingService {
   constructor(private readonly repository: IRepository<ReconciledRecord>) {}
 
+  getDataById(userId: string, key: string): ServiceResult<DataConcilationReport> {
+    const result = this.repository.values();
+
+    if(result.failure) {
+      console.error(result.error);
+      return result;
+    }
+
+    const createReportById = DataConcilationReport.createById(userId, key, result.content!);
+    if (createReportById) {
+      console.error(result.error);
+      return createReportById;
+    }
+
+    return ServiceResult.Succeeded(createReportById);
+  }
+
   public getData(userId: string): ServiceResult<object> {
     const result = this.repository.values();
-    return ServiceResult.Succeeded(result);
+
+    if(result.failure) {
+      console.error(result.error);
+      return result;
+    }
+
+    const createReport = DataConcilationReport.create(userId, result.content!);
+    if (createReport) {
+      console.error(result.error);
+      return createReport;
+    }
+
+    return ServiceResult.Succeeded(createReport);
   }
 }
 
