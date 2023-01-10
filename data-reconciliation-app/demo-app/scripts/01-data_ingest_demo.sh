cfe0cd65c5af1a1ee560549bc383e51301da37a8
Sweta <swetakumari@microsoft.com>
2023-01-10 15:15:33 +0000

    update scripts

--- data-reconciliation-app/demo-app/scripts/01-data_ingest_demo.sh
+++ data-reconciliation-app/demo-app/scripts/01-data_ingest_demo.sh
@@ -9,23 +9,19 @@ VARS_PATH="./demo-app/scripts/env_vars.sh"
 cd "${certs}"
 
 check_eq "Member0 - data ingest succeed" "200" "$(curl $ingestUrl -X POST $(cert_arg member0) -H "Content-Type: application/json" --data-binary "@../../demo-app/data/member0_demo_pt1.json" $only_status_code)"
-addCheckpoint "✅ Member0 successfully ingested data"
 
 check_eq "Member1 - data ingest failed (data is null or empty)" "400" "$(curl $ingestUrl -X POST $(cert_arg member1) -H "Content-Type: application/json" --data-binary "" $only_status_code)"
-addCheckpoint "✅ Application successfully blocked ingesting empty data"
 
 check_eq "Member1 - data ingest succeed" "200" "$(curl $ingestUrl -X POST $(cert_arg member1) -H "Content-Type: application/json" --data-binary "@../../demo-app/data/member1_demo.json" $only_status_code)"
-printf "✅ Member1 successfully ingested data\n"
 
 check_eq "Member2 - data ingest succeed" "200" "$(curl $ingestUrl -X POST $(cert_arg member2) -H "Content-Type: application/json" --data-binary "@../../demo-app/data/member2_demo.json" $only_status_code)"
-addCheckpoint "✅ Member2 successfully ingested data"
+addCheckpoint "🎬 Ingestion Stage Complete"
 
+curl $reportUrl -X GET $(cert_arg member1) | jq '.content[] | select (.group_status == "IN_CONSENSUS")'
+addCheckpoint "🎬 IN_CONSENSUS DATA"
 
-curl $reportUrl -X GET $(cert_arg member0) | jq > ../../demo-app/Reports/Member0_Ingest_Data_V1.json
-addCheckpoint "✅ Member 0 - read data report"
+curl $reportUrl/$id2 -X GET $(cert_arg member0) | jq '. | {content}'
+addCheckpoint "🎬 NOT_ENOUGH_DATA"
 
-curl $reportUrl -X GET $(cert_arg member1) | jq > ../../demo-app/Reports/Member1_Ingest_Data_V1.json
-printf "✅ Member 1 - read data report\n"
-
-curl $reportUrl -X GET $(cert_arg member2) | jq > ../../demo-app/Reports/Member2_Ingest_Data_V1.json
-addCheckpoint "✅ Member 2 - read data report"
+curl $reportUrl/$id3 -X GET $(cert_arg member1) | jq '. | {content}'
+addCheckpoint "🎬 LACK_OF_CONSENSUS DATA"
--- data-reconciliation-app/demo-app/scripts/02-report_change_demo.sh
+++ data-reconciliation-app/demo-app/scripts/02-report_change_demo.sh
@@ -7,14 +7,7 @@ VARS_PATH="./demo-app/scripts/env_vars.sh"
 cd "${certs}"
 
 check_eq "Member0 - data ingest succeed" "200" "$(curl $ingestUrl -X POST $(cert_arg member0) -H "Content-Type: application/json" --data-binary "@../../demo-app/data/member0_demo_pt2.json" $only_status_code)"
-addCheckpoint "✅ Member0 successfully ingested additional data"
+addCheckpoint "🎬 Member0 successfully ingested additional data"
 
-curl $reportUrl -X GET $(cert_arg member0) |jq > ../../demo-app/Reports/Member1_Report_Change_V1.json
-addCheckpoint "✅ Member 0 - read data report"
-
-
-curl $reportUrl -X GET $(cert_arg member1) | jq > ../../demo-app/Reports/Member1_Report_Change_V1.json
-addCheckpoint "✅ Data report changes for Member1"
-
-curl $reportUrl -X GET $(cert_arg member2) | jq > ../../demo-app/Reports/Member2_Report_Change_V1.json
-addCheckpoint "✅ Data report changes for Member2"
+curl $reportUrl/$id -X GET $(cert_arg member1) | jq '. | {content}'
+addCheckpoint "🎬 Data status changes for id: $id for Member1"
