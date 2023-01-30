a88409c791eb26a08d463fdfcd56bae1e9c0da9e
ross-p-smith <rosmith@microsoft.com>
2023-01-30 22:11:41 +0000

    Bicep installed by devcontainer feature

--- scripts/initialise_devContainer.sh
+++ scripts/initialise_devContainer.sh
@@ -5,8 +5,3 @@ set -euo pipefail
 npm install --prefix=banking-app
 npm install --prefix=auditable-logging-app
 npm install --prefix=data-reconciliation-app
-
-# Install Bicep
-curl -Lo bicep https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64
-chmod +x ./bicep
-sudo mv ./bicep /usr/local/bin/bicep
