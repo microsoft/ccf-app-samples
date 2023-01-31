3bca54bed7259cec039c43ba33fc7cf8932bec86
Takuro Sato <takurosato@microsoft.com--username>
2023-01-31 14:47:16 +0000

    Improve messages

--- scripts/test_sandbox.sh
+++ scripts/test_sandbox.sh
@@ -77,7 +77,7 @@ function finish {
         fi
     fi
     kill -9 $sandbox_pid
-    echo "💀 Killed process ${sandbox_pid}"
+    echo "💀 Killed sanbox process ${sandbox_pid}"
 }
 trap finish EXIT
 
