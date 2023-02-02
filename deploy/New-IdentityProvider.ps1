93eb7790056c0276ef888740327d122938ed8b9b
ross-p-smith <rosmith@microsoft.com>
2023-02-02 21:18:11 +0000

    Slimmed down App Reg Creation

--- deploy/New-IdentityProvider.ps1
+++ deploy/New-IdentityProvider.ps1
@@ -12,80 +12,64 @@ param(
     # The name of the AAD API Application
     [Parameter(Mandatory=$false)]
     [string] $AADApiAppName="ccf_demo_api",
-    # Tenant of the Service Principal
-    # TODO
-    [Parameter(Mandatory=$false)]
-    [string] $TenantId="c078b137-176d-4417-b8ee-f566eeed1eb9"
+    # Tenant where the Application Registrations are
+    [Parameter(Mandatory=$true)]
+    [string] $TenantId
 )
+
+# The Magic Strings are well known Ids
+$MSGraphAppId="00000003-0000-0000-c000-000000000000"
+# $Scope_OpenID="37f7f235-527c-4136-accd-4a02d197296e"
+# $Scope_OfflineAccess="7427e0e9-2fba-42fe-b0c0-848c9e6a8182"
+$Scope_UserRead="e1fe6dd8-ba31-4d61-89e7-88639da4683d"
+
+# Hold all of our keys in a hashtable
+$output = @{}
+# Clean our .env file
+Remove-Item ../.env -Force -ErrorAction SilentlyContinue
 
 # Connect with the correct scopes that you need to perform the management operations.
 # These are not the scopes you are granting to the applications.
 $RequiredScopes = @("Directory.AccessAsUser.All", "Directory.ReadWrite.All", "Application.ReadWrite.All")
 Connect-MgGraph -TenantId $TenantId -Scopes $RequiredScopes
 
-# 1. Create the application that secures the API. This needs a secret
+# 1. Create the application that secures the API.
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.graph.applications/new-mgapplication?view=graph-powershell-1.0
-$userImpersonationScopeId=New-Guid
+#$userImpersonationScopeId=New-Guid
 $api_app_manifest = @{
     DisplayName = $AADApiAppName
     SignInAudience = "AzureADMyOrg"
-    API = @{
-        Oauth2PermissionScopes = @(
-            @{
-                Id=$userImpersonationScopeId
-                IsEnabled = true
-                Type = "User"
-                AdminConsentDescription = "Access this API on behalf of a signed in user"
-                AdminConsentDisplayName = "User Impersonation (admin)"
-                UserConsentDescription = "Access this API on behalf of a signed in user"
-                UserConsentDisplayName = "User Impersonation"
-                Value = "user_impersonation"
-            }
-        )
-    }        
-}
-$api_app = New-MgApplication @api_app_manifest
-$api_app | Format-List AppId
-
-# This application needs an Application ID URI so we can uniquely reference it as a Scope
-Update-MgApplication -ApplicationId $api_app.Id -IdentifierUris @("api://$($api_app.AppId)")
-
-# 3. Build the manifest for the Client App (Swagger). Notice that this has user_impersonation
-# access back to the API App
-# The Magic Strings are well known Ids
-$MSGraphAppId="00000003-0000-0000-c000-000000000000"
-$Scope_OpenID="37f7f235-527c-4136-accd-4a02d197296e"
-$Scope_OfflineAccess="7427e0e9-2fba-42fe-b0c0-848c9e6a8182"
-
-$client_params = @{
-    DisplayName = $AADClientAppName
-    Web = @{
-        RedirectUris = "http://localhost:8000/api/docs/oauth2-redirect"
-    }
     RequiredResourceAccess = @{
         ResourceAppId = $MSGraphAppId
         ResourceAccess = @(
             @{
-                Id = $Scope_OpenID
-                Type = "Scope"
-            },
-            @{
-                Id = $Scope_OfflineAccess
+                Id = $Scope_UserRead
                 Type = "Scope"
             })
-        },
-        @{
-        ResourceAppId = $api_app.AppId
+        }
+}
+$api_app = New-MgApplication @api_app_manifest
+$output.Add("ApiApplicationId", $api_app.AppId)
+$output.Add("ApiIdentifierUri", "api://$($api_app.AppId)")
+
+# This application needs an Application ID URI so we can uniquely reference its Scopes
+Update-MgApplication -ApplicationId $api_app.Id -IdentifierUris @($output["ApiIdentifierUri"])
+
+# 2. Build the manifest for the Client App (Swagger).
+$client_params = @{
+    DisplayName = $AADClientAppName
+    RequiredResourceAccess = @{
+        ResourceAppId = $MSGraphAppId
         ResourceAccess = @(
             @{
-                Id = $userImpersonationScopeId
+                Id = $Scope_UserRead
                 Type = "Scope"
-            }
-        )
+            })
         }
     SignInAudience = "AzureADMyOrg"
 }
 $client_app = New-MgApplication @client_params
+$output.Add("ClientApplicationId", $client_app.AppId)
 
 # 2. Now add a password and it will be returned to you, only the first time around.
 $passwordCred = @{
@@ -95,8 +79,12 @@ $passwordCred = @{
 
 # https://learn.microsoft.com/en-us/powershell/module/az.resources/new-azadappcredential?view=azps-9.3.0#examples
 $secret = Add-MgApplicationPassword -applicationId $client_app.Id -PasswordCredential $passwordCred
-$secret | Format-List SecretText, EndDateTime
+$output.Add("ClientSecret", $secret.SecretText)
 
+# Output hastable as key=value pairs in a file
+$output.GetEnumerator() | ForEach-Object {
+    Add-Content ../.env "$($_.Key)=$($_.Value)"
+}
 
+# Destroy any cookies/connections
 Disconnect-MgGraph
-
