b4f9ed47cb82201233db5cef1a313cd6a59916c9
Aymalla <Aymalla@outlook.com>
2023-02-16 11:48:30 +0000

    solve time out issue

--- scripts/devContainer_OnInitialize.sh
+++ scripts/devContainer_OnInitialize.sh
@@ -2,5 +2,4 @@
 set -euo pipefail
 
 # Create the JWT issuer config files for (Test - Microsoft Azure Identity Provider).
-npm install --prefix=data-reconciliation-app
 npm run create-jwt-config --prefix=data-reconciliation-app
