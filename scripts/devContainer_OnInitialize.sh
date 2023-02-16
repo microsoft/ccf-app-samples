ca7ffd9df4cfd3b30cad2db2766dd4b82f22a3bc
Aymalla <Aymalla@outlook.com>
2023-02-16 11:25:33 +0000

    restore packages

--- scripts/devContainer_OnInitialize.sh
+++ scripts/devContainer_OnInitialize.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+./devContainer_OnStart.sh
+
 # Create the JWT issuer config files for (Test - Microsoft Azure Identity Provider).
 npm run create-jwt-config --prefix=data-reconciliation-app
