19bb8425fc4c8ec0b3efa0d65dc4092f29c07686
Aymalla <Aymalla@outlook.com>
2023-02-16 12:12:53 +0000

    return the certificate as string

--- scripts/devContainer_OnInitialize.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# Create the JWT issuer config files for (Test - Microsoft Azure Identity Provider).
-npm run create-jwt-config --prefix=data-reconciliation-app
--- scripts/devContainer_OnStart.sh
+++ scripts/devContainer_OnStart.sh
@@ -5,3 +5,6 @@ set -euo pipefail
 npm install --prefix=banking-app
 npm install --prefix=auditable-logging-app
 npm install --prefix=data-reconciliation-app
+
+# Create the JWT issuer config files for (Test - Microsoft Azure Identity Provider).
+npm run create-jwt-config --prefix=data-reconciliation-app
